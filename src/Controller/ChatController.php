877a9c09721e43478011ed07b5d371248aa41843
Lucie <luciebrochet358@gmail.com>
2019-10-10 13:50:13 +0200

    starting with mercure

--- src/Controller/ChatController.php
+++ src/Controller/ChatController.php
@@ -2,7 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Service\MercureCookieGenerator;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Mercure\Publisher;
+use Symfony\Component\Mercure\Update;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ChatController extends AbstractController
@@ -10,12 +14,36 @@ class ChatController extends AbstractController
     /**
      * @Route("/chat", name="chat")
      */
-    public function index()
+    public function index(MercureCookieGenerator $mercureCookieGenerator)
     {
-        return $this->render('chat/index.html.twig', [
+        $response = $this->render('chat/index.html.twig', [
             'config' => [
                 'topic' => 'chat',
                 'publishRoute' => $this->generateUrl('publisher', ['topic' => 'chat'])
+            ]
+        ]);
+
+        $response->headers->set('set-cookie', $mercureCookieGenerator->generate($this->getUser()));
+        return $response;
+    }
+
+    /**
+     * @Route("/chat/message/{user}", name="postMessage", methods={"POST"})
+     */
+    public function postMessage(Publisher $publisher, ?User $user = null)
+    {
+        $targets = [];
+        if ($user !== null) {
+            $targets = ["http://o-stan.fr/user/{$user->getId()}"];
+        }
+
+        $update = new Update("http://o-stan.fr/chat", "[]", $targets);
+        $publisher($update);
+
+        return $this->render('chat/index.html.twig', [
+            'config' => [
+                'topic' => 'http://o-stan.fr/chat',
+                'publishRoute' => $this->generateUrl('publisher', ['topic' => 'http://o-stan.fr/chat'])
             ]
         ]);
     }
